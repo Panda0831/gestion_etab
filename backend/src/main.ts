01a6738041440085eb2f37e12bca0ed74db7e292
Nam🐼 <189771607+HarimalalaEricka@users.noreply.github.com>
2026-08-08 22:01:59 +0300

    Detection automatique des changements de fichiers
    Detection automatique des changements de fichiers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('Test hot reload');
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
